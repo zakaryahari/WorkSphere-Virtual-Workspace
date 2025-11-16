f9cabbe443f23b9759fc1386e66f09afff98d4c2
zakarya hari <zakariahari42@gmail.com>
2025-11-17 00:59:10 +0100

    Ajouter un listener (event delegation) a la form de add employee , est Verifier input de nom.

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ if (add_employee_form) {
   let nom_regex = /^[A-Za-z]+(?:[\sA-Za-z]){2,}$/;
   let email_regex = /^[\w-]+[\w.-]+\@+[\w]+\.+[\w.]$/;
   let phone_regex = /^\+?\+212+\d{9,10}$/;
+
+  add_employee_form.addEventListener('input', (e) => {
+    const element_input = e.target;
+    if (element_input.id === 'employee_nom_input') {
+      if (nom_regex.test(element_input.value)) {
+        employee_nom_msg_error.textContent = '';
+      }
+      if (!nom_regex.test(element_input.value) && element_input.value !== '') {
+        const employee_nom_msg_error = document.getElementById('employee_nom_msg_error');
+        employee_nom_msg_error.textContent = 'Invalide nom input';
+      }
+      if (element_input.value === '') {
+        employee_nom_msg_error.textContent = '';
+      }
+    }
+  });
 }
