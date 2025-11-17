ba11056c10ce77d316db88055dab3db283e449a5
zakarya hari <zakariahari42@gmail.com>
2025-11-17 01:27:52 +0100

    Cree d'une founction qui a checker le regex de nom , email , telephone.

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const add_employee_form = document.querySelector('#add_employee_form');
 
+function Valide_input_regex(inputElement, regex, span_id, msg_error) {
+  const employee_nom_msg_error = document.getElementById(span_id);
+  if (regex.test(inputElement.value)) {
+    employee_nom_msg_error.textContent = '';
+  }
+  if (!regex.test(inputElement.value) && inputElement.value !== '') {
+    employee_nom_msg_error.textContent = msg_error;
+  }
+  if (inputElement.value === '') {
+    employee_nom_msg_error.textContent = '';
+  }
+}
+
 if (add_employee_form) {
+  const nom_msg_error = 'Please enter a valid name.';
+  const email_msg_error = 'Please enter a valid Email.';
+  const phone_msg_error = 'Please enter a valid phone number (10 digits).';
+
+  const nom_spanId = 'employee_nom_msg_error';
+  const email_spanId = 'employee_email_msg_error';
+  const phone_spanId = 'employee_tel_msg_error';
+
   let nom_regex = /^[A-Za-z]+(?:[\sA-Za-z]){2,}$/;
-  let email_regex = /^[\w-]+[\w.-]+\@+[\w]+\.+[\w.]$/;
-  let phone_regex = /^\+?\+212+\d{9,10}$/;
+  let email_regex = /^[\w.-]+@[\w.-]+\.[A-Za-z]{2,}$/;
+  let phone_regex = /^(?:\+212|0|6)+\d{8,9}$/;
 
   add_employee_form.addEventListener('input', (e) => {
     const element_input = e.target;
     if (element_input.id === 'employee_nom_input') {
-      if (nom_regex.test(element_input.value)) {
-        employee_nom_msg_error.textContent = '';
-      }
-      if (!nom_regex.test(element_input.value) && element_input.value !== '') {
-        const employee_nom_msg_error = document.getElementById('employee_nom_msg_error');
-        employee_nom_msg_error.textContent = 'Invalide nom input';
-      }
-      if (element_input.value === '') {
-        employee_nom_msg_error.textContent = '';
-      }
+      Valide_input_regex(element_input, nom_regex, nom_spanId, nom_msg_error);
+    }
+    if (element_input.id === 'employee_email_input') {
+      Valide_input_regex(element_input, email_regex, email_spanId, email_msg_error);
+    }
+    if (element_input.id === 'employee_tel_input') {
+      Valide_input_regex(element_input, phone_regex, phone_spanId, phone_msg_error);
     }
   });
 }
